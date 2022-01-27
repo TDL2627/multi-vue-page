2496d112da0ba6e8f8276ac8965e54f64ad4e4fb
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-27 12:13:23 +0200

    removed hello world component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,19 @@
 <!-- <Navbar/> -->
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+
 import Navbar from '@/components/Navbar.vue';
 
 export default {
 
   components: {
-    HelloWorld,Navbar
+    Navbar
   }
 }
 </script>
