14f9697d0849710b570c2503fe9341cab880119c
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-27 20:40:57 +0200

    added bootstrap

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,33 @@
 <template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" >Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+
+      
+      </ul>
+  
+    </div>
+  </div>
+</nav>
 <div id="nav">
  <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/experience">Experience</router-link> |
-    <router-link to="/skills">Skills</router-link> |
-    <router-link to="/projects">Projects</router-link> |
-    <router-link to="/testimonials">Testimonials</router-link> |
-      <router-link to="/contact">Contact</router-link>
+    <router-link  to="/about"><p>About</p></router-link> |
+    <router-link  to="/experience">Experience</router-link> |
+    <router-link  to="/skills">Skills</router-link> |
+    <router-link  to="/projects">Projects</router-link> |
+    <router-link   to="/testimonials">Testimonials</router-link> |
+      <router-link   to="/contact">Contact</router-link>
        
 </div>
  
@@ -19,13 +40,18 @@ export default {
 </script>
 
 <style scoped>
+.nav-linker{
+  color:white;
+}
 #nav {
   padding: 30px;
+  background-color: #9d4edd;
+  color: white;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: black;
 }
 
 
--- src/main.js
+++ src/main.js
@@ -3,3 +3,6 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+import "bootstrap/dist/css/bootstrap.css"
+
+import "bootstrap/dist/js/bootstrap.js"
